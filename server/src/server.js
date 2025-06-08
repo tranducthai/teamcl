70dca1043bdca010271082bbc7d3205a3f222937
tranducthai <147269582+tranducthai@users.noreply.github.com>
2025-06-09 01:36:15 +0700

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -4,39 +4,18 @@ import { StatusCodes } from "http-status-codes";
 import cookieParser from "cookie-parser";
 import express, { json } from "express";
 import { createServer } from "http";
-// import fs from "fs";
-// import path from "path";
-// import { fileURLToPath } from "url";
 
 import apiRouter from "./api/routes/index.js";
 import setupSocket from "./socket/index.js";
 import embeddingProvider from "./config/embedding-provider.js";
 import { handleApiError } from "./middlewares/error-middleware.js";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// const certPath = path.join(__dirname, "../certificates", "localhost.pem");
-// const keyPath = path.join(__dirname, "../certificates", "localhost-key.pem");
-
-/**
- * Starts the server with Express and HTTP.
- *
- * @function startServer
- * @description Initializes an Express application, sets up an HTTP server, and configures routes.
- */
 const startServer = () => {
-  // const httpsOptions = {
-  //   key: fs.readFileSync(keyPath),
-  //   cert: fs.readFileSync(certPath)
-  // };
   const app = express();
   const server = createServer(app);
-  // const server = createServer(httpsOptions, app);
 
   setupSocket(server);
 
-  // Middlewares
   app.use(json());
   app.use(cookieParser());
   app.use(
@@ -63,8 +42,11 @@ const startServer = () => {
     });
   });
 
-  server.listen(process.env.APP_PORT || 8080, () => {
-    console.log(`Server is running at http://${process.env.APP_HOST}:${process.env.APP_PORT}`);
+  const PORT = process.env.APP_PORT || 8080;
+  const HOST = process.env.APP_HOST || "localhost";
+
+  server.listen(PORT, () => {
+    console.log(`Server is running at http://${HOST}:${PORT}`);
   });
 };
 
@@ -82,6 +64,6 @@ const startServer = () => {
     startServer();
   } catch (error) {
     console.error(error);
-    process.exit(0);
+    process.exit(1);
   }
 })();
